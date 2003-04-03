f98b737836449d1c39a0685d45dca0f99b078343
Mark Rivers <MarkRivers@users.noreply.github.com>
2003-04-03 04:23:36 +0000

    Added support for interrupts on the IP-Unidig-I series. Added additional arguments to IpUnidig::init and IpUnidig::IpUnidig to support this.

--- ipUnidigApp/src/ipUnidig.cc
+++ ipUnidigApp/src/ipUnidig.cc
@@ -12,6 +12,11 @@ of this distribution.
 
     This is the driver for the Greenspring IP-Unidig series of digital I/O IP
     modules.  It also supports the Systran DIO316I module.
+
+    Modifications:
+    1-Apr-2003  MLR  Added support for interrupts on the IP-Unidig-I series.
+                     Added additional arguments to IpUnidig::init and
+                     IpUnidig::IpUnidig to support this.
 */
 
 #include <vxWorks.h>
@@ -20,8 +25,11 @@ of this distribution.
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
- 
+
+#include "epicsPrint.h"
+
 #include "IndustryPackModule.h"
+#include "Reboot.h"
 #include "ipUnidig.h"
 
 #define GREENSPRING_ID 0xF0
@@ -56,14 +64,15 @@ extern "C"
 #ifdef NODEBUG
 #define DEBUG(l,f,v...) ;
 #else
-#define DEBUG(l,f,v...) { if(l<=IpUnidigDebug) printf(f,## v); }
+#define DEBUG(l,f,v...) { if(l<=IpUnidigDebug) epicsPrintf(f,## v); }
 #endif
 volatile int IpUnidigDebug = 0;
 }
 
 
 IpUnidig * IpUnidig::init(
-    const char *moduleName, const char *carrierName, const char *siteName)
+    const char *moduleName, const char *carrierName, const char *siteName,
+    int intVec, int risingMask, int fallingMask, int maxClients)
 {
     IndustryPackModule *pIPM = IndustryPackModule::createIndustryPackModule(
         moduleName,carrierName,siteName);
@@ -110,26 +119,38 @@ IpUnidig * IpUnidig::init(
        return(0);
        break;
     }
-    IpUnidig *pIpUnidig = new IpUnidig(pIPM, manufacturer, model);
+    IpUnidig *pIpUnidig = new IpUnidig(pIPM, manufacturer, model, 
+                                 intVec, risingMask, fallingMask, maxClients);
     return(pIpUnidig);
 }
 
 IpUnidig::IpUnidig(IndustryPackModule *pIndustryPackModule, 
-                   unsigned char manufacturer, unsigned char model)
-: pIPM(pIndustryPackModule), manufacturer(manufacturer), model(model)
+                   unsigned char manufacturer, unsigned char model, 
+                   int intVec, int risingMask, int fallingMask, int maxClients)
+: pIPM(pIndustryPackModule), manufacturer(manufacturer), model(model),
+                             rebooting(0), maxClients(maxClients), numClients(0)
 {
    // Set up the register pointers.  Set the defaults for most modules
    // Define registers in units of 16-bit words
 
     UINT16 *base = (UINT16 *) pIPM->getMemBaseIO();
-    outputRegisterLow  = base;
-    outputRegisterHigh = base + 0x1;
-    inputRegisterLow   = base + 0x2;
-    inputRegisterHigh  = base + 0x3;
-    outputEnableLow    = base + 0x8;
-    outputEnableHigh   = base + 0xa;
-    controlRegister0   = base + 0xc;
-    DACRegister        = base + 0xe;
+    outputRegisterLow        = base;
+    outputRegisterHigh       = base + 0x1;
+    inputRegisterLow         = base + 0x2;
+    inputRegisterHigh        = base + 0x3;
+    outputEnableLow          = base + 0x8;
+    outputEnableHigh         = base + 0x5;
+    controlRegister0         = base + 0x6;
+    intVecRegister           = base + 0x8;
+    intEnableRegisterLow     = base + 0x9;
+    intEnableRegisterHigh    = base + 0xa;
+    intPolarityRegisterLow   = base + 0xb;
+    intPolarityRegisterHigh  = base + 0xc;
+    intClearRegisterLow      = base + 0xd;
+    intClearRegisterHigh     = base + 0xe;
+    intPendingRegisterLow    = base + 0xd;
+    intPendingRegisterHigh   = base + 0xe;
+    DACRegister              = base + 0xe;
     
     // Set things up for specific models which need to be treated differently
     switch (manufacturer) {
@@ -173,13 +194,61 @@ IpUnidig::IpUnidig(IndustryPackModule *pIndustryPackModule,
                     manufacturer);
        break;
     }
+    switch (model) {
+       case UNIDIG_I_O_24I:
+       case UNIDIG_I_E:
+       case UNIDIG_I:
+       case UNIDIG_I_D:
+       case UNIDIG_I_O_24IO:
+       case UNIDIG_I_HV_16I8O:
+       case UNIDIG_I_O_12I12O:
+       case UNIDIG_I_HV_8I16O:
+          supportsInterrupts = TRUE;
+          break;
+       default:
+          supportsInterrupts = FALSE;
+          break;
+    }
+    if (maxClients < 5) maxClients = 5;
+    client = (IpUnidigClient *) calloc(maxClients, sizeof(IpUnidigClient));
+    this->risingMask = risingMask;
+    this->fallingMask = fallingMask;
+    this->polarityMask = risingMask;
+    // If the interrupt vector is zero, or if both masks are zero don't
+    // bother with interrupts, since the user probably didn't pass these
+    // parameters to IpUnidig::init().  These are optional parameters added
+    // after initial release.
+    if (supportsInterrupts && 
+        (intVec !=0) && 
+        ((risingMask | fallingMask) != 0)) {
+       // Interrupt support
+       // Write to the interrupt polarity and enable registers
+       if (fppProbe()==OK)
+          pFpContext = (FP_CONTEXT *)calloc(1, sizeof(FP_CONTEXT));
+       else
+          pFpContext = NULL;
+       *intVecRegister = intVec;
+       if (intConnect(INUM_TO_IVEC(intVec),(VOIDFUNCPTR)intFunc,(int)this)==ERROR){
+                printf("ipUnidig intConnect Failure\n");
+       }
+       *intPolarityRegisterLow  = (UINT16) polarityMask;
+       *intPolarityRegisterHigh = (UINT16) (polarityMask >> 16);
+       *intEnableRegisterLow  = (UINT16) (risingMask | fallingMask);
+       *intEnableRegisterHigh = (UINT16) ((risingMask | fallingMask) >> 16);
+       Reboot::rebootHookAdd(rebootCallback,(void *)this);
+       pIPM->intConfig(0);
+       pIPM->intConfig(1);
+       pIPM->intEnable(0);
+    }
 }
+
 
 int IpUnidig::setBits(UINT32 mask)
 {
     // For the IP-Unidig differential output models, must enable outputs
     // Don't do this for all outputs in constructor, since then nothing could
     // be an input
+    if(rebooting) taskSuspend(0);
     if ((manufacturer == GREENSPRING_ID)  &&
         ((model == UNIDIG_D) || (model == UNIDIG_I_D))) {
          *outputEnableLow  |= (UINT16) mask;
@@ -187,8 +256,8 @@ int IpUnidig::setBits(UINT32 mask)
     }
     if (outputRegisterLow)  *outputRegisterLow  |= (UINT16) mask;
     if (outputRegisterHigh) *outputRegisterHigh |= (UINT16) (mask >> 16);
-    DEBUG(1, "IpUnidig::setBits, mask=%x\n", mask);
-    DEBUG(2, "IpUnidig::setBits, outputRegisterLow=%p, outputRegisterHigh=%p\n", 
+    DEBUG(5, "IpUnidig::setBits, mask=%x\n", mask);
+    DEBUG(6, "IpUnidig::setBits, outputRegisterLow=%p, outputRegisterHigh=%p\n", 
               outputRegisterLow, outputRegisterHigh);
     return(0);
 }
@@ -198,6 +267,7 @@ int IpUnidig::clearBits(UINT32 mask)
     // For the IP-Unidig differential output models, must enable outputs
     // Don't do this for all outputs in constructor, since then nothing could
     // be an input
+    if(rebooting) taskSuspend(0);
     if ((manufacturer == GREENSPRING_ID)  &&
         ((model == UNIDIG_D) || (model == UNIDIG_I_D))) {
          *outputEnableLow  |= (UINT16) mask;
@@ -205,19 +275,20 @@ int IpUnidig::clearBits(UINT32 mask)
     }
     if (outputRegisterLow)  *outputRegisterLow  &= (UINT16) ~mask;
     if (outputRegisterHigh) *outputRegisterHigh &= (UINT16) (~mask >> 16);
-    DEBUG(1, "IpUnidig::clearBits, mask=%x\n", mask);
-    DEBUG(2, "IpUnidig::clearBits, outputRegisterLow=%p, outputRegisterHigh=%p\n", 
+    DEBUG(5, "IpUnidig::clearBits, mask=%x\n", mask);
+    DEBUG(6, "IpUnidig::clearBits, outputRegisterLow=%p, outputRegisterHigh=%p\n", 
               outputRegisterLow, outputRegisterHigh);
     return(0);
 }
 
 int IpUnidig::readBits(UINT32 *value)
 {
+    if(rebooting) taskSuspend(0);
     *value = 0;
     if (inputRegisterLow)  *value  = (UINT32) *inputRegisterLow;
     if (inputRegisterHigh) *value |= (UINT32) (*inputRegisterHigh << 16);
-    DEBUG(1, "IpUnidig::readBits, *value=%x\n", *value);
-    DEBUG(2, "IpUnidig::readBits, inputRegisterLow=%p, inputRegisterHigh=%p\n", 
+    DEBUG(5, "IpUnidig::readBits, *value=%x\n", *value);
+    DEBUG(6, "IpUnidig::readBits, inputRegisterLow=%p, inputRegisterHigh=%p\n", 
               inputRegisterLow, inputRegisterHigh);
     return(0);
 }
@@ -235,3 +306,68 @@ int IpUnidig::setDAC(UINT16 value)
     } 
 }
 
+int IpUnidig::registerCallback(IpUnidigCallback callback, void *pvt, int mask)
+   {
+   IpUnidigClient *pClient;
+   if(rebooting) taskSuspend(0);
+   DEBUG(1,"IpUnidig::registerCallack, callback=%p, pvt=%p\n", callback, pvt);
+   if (numClients >= maxClients) {
+      printf("IpUnidig::registerClient: too many clients\n");
+      return(-1);
+   }
+   // Disable interrupts while adding this callback
+   int intKey = intLock();
+   numClients++;
+   pClient = &client[numClients-1];
+   pClient->pvt = pvt;
+   pClient->callback = callback;
+   pClient->mask = mask;
+   intUnlock(intKey);
+   return(0);
+}
+
+void IpUnidig:: intFunc(void *v)
+{
+   IpUnidig *t = (IpUnidig *) v;
+   IpUnidigClient *pClient;
+   int    i;
+   UINT32 inputs=0, pendingLow, pendingHigh, pendingMask, invertMask;
+
+   // Save and restore FP registers so application interrupt functions can do
+   // floating point operations.  Skip if no fpp hardware present.
+   if (t->pFpContext != NULL) fppSave (t->pFpContext);
+   // Clear the interrupts by copying from the interrupt pending register to the
+   // interrupt clear register
+   *t->intClearRegisterLow = pendingLow = *t->intPendingRegisterLow;
+   *t->intClearRegisterHigh = pendingHigh = *t->intPendingRegisterHigh;
+   pendingMask = pendingLow | (pendingHigh << 16);
+   // Read the current input.  Don't use readBits because that can print debugging.
+   if (t->inputRegisterLow)  inputs = (UINT32) *t->inputRegisterLow;
+   if (t->inputRegisterHigh) inputs |= (UINT32) (*t->inputRegisterHigh << 16);
+   // Call the callback routines which have registered
+   for (i = 0; i < t->numClients; i++) {
+      pClient = &t->client[i];
+      if ((pendingMask & pClient->mask) != 0) {
+         pClient->callback(pClient->pvt, inputs);
+      }
+   }
+   // Are there any bits which should generate interrupts on both the rising
+   // and falling edge, and which just generated this interrupt?
+   invertMask = pendingMask & t->risingMask & t->fallingMask;
+   if (invertMask != 0) {
+      // We want to invert all bits in the polarityMask that are set in 
+      // invertMask. This is done with xor.
+      t->polarityMask = t->polarityMask ^ invertMask;
+      *t->intPolarityRegisterLow  = (UINT16) t->polarityMask;
+      *t->intPolarityRegisterHigh = (UINT16) (t->polarityMask >> 16);
+   }
+   if (t->pFpContext != NULL) fppRestore(t->pFpContext);
+}
+
+void IpUnidig:: rebootCallback(void *v)
+{
+   IpUnidig *pIpUnidig = (IpUnidig *)v;
+   *pIpUnidig->intEnableRegisterLow = 0;
+   *pIpUnidig->intEnableRegisterHigh = 0;
+   pIpUnidig->rebooting = true;
+}
