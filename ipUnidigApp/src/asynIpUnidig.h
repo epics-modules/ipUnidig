65f1d99b13706fc10f4fcd38b15d9429eca58ce3
Mark Rivers <MarkRivers@users.noreply.github.com>
2004-07-30 03:38:17 +0000

    Removed most functions, now in asynUInt32Callback* in asyn

--- ipUnidigApp/src/asynIpUnidig.h
+++ ipUnidigApp/src/asynIpUnidig.h
@@ -2,12 +2,9 @@
 
     Author: Mark Rivers
     Date: 29-June-2004
-  
-    Modifications:
-    1-Apr-2003  MLR  Changes to support interrupts
-   23-Apr-2003  MLR  Added functions for changing the rising and falling masks
-   27-May-2003  MLR  Converted to EPICS R3.14
-   29-Jun-2004  MLR  Converted from MPF to asyn
+
+    Almost all functions of the driver are now in the generic asynUInt32Digital and 
+    asynUInt32DigitalCallback interfaces.  Only setDAC remains here.
 */
 
 #ifndef asynIpUnidigH
@@ -15,33 +12,11 @@
 
 #include <asynDriver.h>
 
-typedef void (*ipUnidigCallback)(void *pvt, epicsUInt32 data);
-
 /* Interface supported by ipUnidigAsyn drivers. */
 #define asynIpUnidigType "asynIpUnidig"
 typedef struct {
-    asynStatus (*setBits)          (void *drvPvt, asynUser *pasynUser, 
-                                    epicsUInt32 mask);
-    asynStatus (*clearBits)        (void *drvPvt, asynUser *pasynUser, 
-                                    epicsUInt32 mask);
     asynStatus (*setDAC)           (void *drvPvt, asynUser *pasynUser, 
                                     epicsUInt16 value);
-    epicsUInt32 (*getRisingMask)   (void *drvPvt, asynUser *pasynUser);
-    void (*setRisingMaskBits)      (void *drvPvt, asynUser *pasynUser, 
-                                    epicsUInt32 mask);
-    void (*clearRisingMaskBits)    (void *drvPvt, asynUser *pasynUser, 
-                                    epicsUInt32 mask);
-    epicsUInt32 (*getFallingMask)  (void *drvPvt, asynUser *pasynUser);
-    void (*setFallingMaskBits)     (void *drvPvt, asynUser *pasynUser, 
-                                    epicsUInt32 mask);
-    void (*clearFallingMaskBits)   (void *drvPvt, asynUser *pasynUser, 
-                                    epicsUInt32 mask);
-    asynStatus (*registerCallback) (void *drvPvt, asynUser *pasynUser, 
-                                    ipUnidigCallback callback, epicsUInt32 mask,
-                                    void *pvt);
-    asynStatus (*cancelCallback)   (void *drvPvt, asynUser *pasynUser, 
-                                    ipUnidigCallback callback, epicsUInt32 mask,
-                                    void *pvt);
 } asynIpUnidig;
 
 #endif /* asynIpUnidigH */
